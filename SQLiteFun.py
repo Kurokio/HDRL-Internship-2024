a560905b6a81ff51f9ea7eb5d917892b7d57cf71
Zach Boquet <zcboquet@gmail.com>
2024-06-17 21:53:17 +0000

    Created python script that contains needed SQLite functions to be called from main notebook

---
+++ SQLiteFun.py
@@ -0,0 +1,90 @@
+import sqlite3
+
+# add table to existing db
+def create_tables():
+    sql_statements = (
+        """CREATE TABLE IF NOT EXISTS MetadataEntries (
+                rowNum INTEGER PRIMARY KEY, 
+                SPASE_id TEXT NOT NULL, 
+                author TEXT,
+                authorRole TEXT,
+                publisher TEXT,
+                publicationYr TEXT,
+                dataset TEXT,
+                license TEXT,
+                URL TEXT,
+                prodKey TEXT,
+                description TEXT,
+                PI TEXT
+        );""",
+        """CREATE TABLE IF NOT EXISTS MetadataSources (
+                rowNum INTEGER PRIMARY KEY, 
+                SPASE_id TEXT NOT NULL UNIQUE, 
+                author_source TEXT,
+                publisher_source TEXT,
+                publication_yr_source TEXT,
+                dataset_source TEXT,
+                license_source TEXT,
+                datalink_source TEXT,
+                description_source TEXT,
+                PI_source TEXT
+        );"""
+        """CREATE TABLE IF NOT EXISTS TestResults (
+                rowNum INTEGER PRIMARY KEY, 
+                SPASE_id TEXT NOT NULL UNIQUE, 
+                FAIR_Score INTEGER,
+                FAIR_ScoreDate TEXT,
+                MostRecent TEXT
+        );"""
+        """CREATE TABLE IF NOT EXISTS Records (
+                rowNum INTEGER PRIMARY KEY, 
+                SPASE_id TEXT NOT NULL UNIQUE, 
+                SPASE_Version INTEGER,
+                LastModified TEXT,
+                SPASE_URL TEXT
+        );""")
+    # create a database connection
+    try:
+        with sqlite3.connect('SPASE_Data.db') as conn:
+            cursor = conn.cursor()
+            for statement in sql_statements:
+                cursor.executescript(statement)
+
+            conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+# add entries/rows to table
+def add_Metadata(conn, entry):
+    sql = '''INSERT INTO MetadataEntries(SPASE_id,author,authorRole,publisher,publicationYr,dataset,
+            license,URL,prodKey,description,PI)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+    return cur.lastrowid
+
+def add_Sources(conn, entry):
+    sql = '''INSERT INTO MetadataSources(SPASE_id,author_source,publisher_source,
+            publication_yr_source,dataset_source,license_source,datalink_source,description_source,PI_source)
+            VALUES(?,?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+    return cur.lastrowid
+
+def add_TestResults(conn, entry):
+    sql = '''INSERT INTO TestResults(SPASE_id,FAIR_Score,FAIR_ScoreDate,MostRecent)
+            VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+    return cur.lastrowid
+
+def add_Records(conn, entry):
+    sql = '''INSERT INTO Records(SPASE_id, SPASE_Version,LastModified,SPASE_URL)
+            VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+    return cur.lastrowid
