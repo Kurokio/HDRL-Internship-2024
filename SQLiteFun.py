62219d991304189d15773e85a1ef2d50c3cb1bd5
Zach Boquet <zcboquet@gmail.com>
2024-06-21 21:57:11 +0000

    Adding docstrings, comments, and a function list. Beginning to add code for another table to signify which records pass the defined tests in the Intern Doc

--- SQLiteFun.py
+++ SQLiteFun.py
@@ -1,7 +1,21 @@
+""" Function List:
+create_tables: Creates the MetadataEntries, MetadataSources, TestResults, and Records tables
+add_Metadata: Inserts values into the MetadataEntries table and returns its row number
+add_Sources: Inserts values into the MetadataSources table and returns its row number
+add_TestResults: Inserts values into the TestResults table and returns its row number
+add_Records: Inserts values into the Records table and returns its row number
+execution: Executes a given SQLite statement and returns the result in a list
+"""
+
 import sqlite3
 
 # add table to existing db
 def create_tables():
+    """Connects to the given SQLite database, creates a cursor object, and calls the executescript method 
+    with the sql_statements argument. These changes are then committed which creates the MetadataEntries, 
+    MetadataSources, TestResults, and Records tables in the specified database. This also displays error 
+    messages if any arise."""
+    
     sql_statements = (
         """CREATE TABLE IF NOT EXISTS MetadataEntries (
                 rowNum INTEGER PRIMARY KEY, 
@@ -43,7 +57,7 @@ def create_tables():
                 LastModified TEXT,
                 SPASE_URL TEXT
         );""")
-    # create a database connection
+    # create a database connection, executes statement, and commits the changes to db
     try:
         with sqlite3.connect('SPASE_Data.db') as conn:
             cursor = conn.cursor()
@@ -56,6 +70,20 @@ def create_tables():
 
 # add entries/rows to table
 def add_Metadata(conn, entry):
+    """Initializes a SQLite INSERT statement with ? serving as placeholders to be replaced by the values 
+    specified in the entry parameter. Connects to the given SQLite database, creates a cursor object, 
+    and calls the execute method with the sql and entry arguments. These changes are then committed which 
+    inserts the values provided into the fields specified such as SPASE_id, author, etc. in the MetadataEntries 
+    table. It finally returns the row number inserted into the table.
+    
+    :param conn: A string of the SQLite database that holds the table to insert into.
+    :type conn: String
+    :param entry: A tuple of the values being inserted into the table.
+    :type entry: tuple
+    :return: The row number of the data that was inserted.
+    :rtype: int
+    """
+    
     sql = '''INSERT INTO MetadataEntries(SPASE_id,author,authorRole,publisher,publicationYr,dataset,
             license,URL,prodKey,description,PI)
             VALUES(?,?,?,?,?,?,?,?,?,?,?) '''
@@ -65,6 +93,20 @@ def add_Metadata(conn, entry):
     return cur.lastrowid
 
 def add_Sources(conn, entry):
+    """Initializes a SQLite INSERT statement with ? serving as placeholders to be replaced by the values 
+    specified in the entry parameter. Connects to the given SQLite database, creates a cursor object, 
+    and calls the execute method with the sql and entry arguments. These changes are then committed which 
+    inserts the values provided into the fields specified such as SPASE_id, author_source, etc. in the 
+    MetadataSources table. It finally returns the row number inserted into the table.
+    
+    :param conn: A string of the SQLite database that holds the table to insert into.
+    :type conn: String
+    :param entry: A tuple of the values being inserted into the table.
+    :type entry: tuple
+    :return: The row number of the data that was inserted.
+    :rtype: int
+    """
+    
     sql = '''INSERT INTO MetadataSources(SPASE_id,author_source,publisher_source,
             publication_yr_source,dataset_source,license_source,datalink_source,description_source,PI_source)
             VALUES(?,?,?,?,?,?,?,?,?) '''
@@ -74,6 +116,20 @@ def add_Sources(conn, entry):
     return cur.lastrowid
 
 def add_TestResults(conn, entry):
+    """Initializes a SQLite INSERT statement with ? serving as placeholders to be replaced by the values 
+    specified in the entry parameter. Connects to the given SQLite database, creates a cursor object, 
+    and calls the execute method with the sql and entry arguments. These changes are then committed which 
+    inserts the values provided into the fields specified such as SPASE_id, FAIR_Score, etc. in the 
+    TestResults table. It finally returns the row number inserted into the table.
+    
+    :param conn: A string of the SQLite database that holds the table to insert into.
+    :type conn: String
+    :param entry: A tuple of the values being inserted into the table.
+    :type entry: tuple
+    :return: The row number of the data that was inserted.
+    :rtype: int
+    """
+    
     sql = '''INSERT INTO TestResults(SPASE_id,FAIR_Score,FAIR_ScoreDate,MostRecent)
             VALUES(?,?,?,?) '''
     cur = conn.cursor()
@@ -82,6 +138,20 @@ def add_TestResults(conn, entry):
     return cur.lastrowid
 
 def add_Records(conn, entry):
+    """Initializes a SQLite INSERT statement with ? serving as placeholders to be replaced by the values 
+    specified in the entry parameter. Connects to the given SQLite database, creates a cursor object, 
+    and calls the execute method with the sql and entry arguments. These changes are then committed which 
+    inserts the values provided into the fields specified such as SPASE_id, SPASE_Version, etc. in the 
+    Records table. It finally returns the row number inserted into the table.
+    
+    :param conn: A string of the SQLite database that holds the table to insert into.
+    :type conn: String
+    :param entry: A tuple of the values being inserted into the table.
+    :type entry: tuple
+    :return: The row number of the data that was inserted.
+    :rtype: int
+    """
+    
     sql = '''INSERT INTO Records(SPASE_id, SPASE_Version,LastModified,SPASE_URL)
             VALUES(?,?,?,?) '''
     cur = conn.cursor()
@@ -89,12 +159,22 @@ def add_Records(conn, entry):
     conn.commit()
     return cur.lastrowid
 
-# executes given SQLite statement
+# executes given SQLite SELECT statement
 def execution(stmt):
+    """Connects to the given SQLite database, creates a cursor object, and calls the execute method 
+    with the stmt argument. Calls the fetchall method to get all rows returned by the statement that 
+    was executed. This also displays error messages if any arise. Lastly, it returns the results of 
+    the SQLite statement in a list
+    
+    :param stmt: A string of the SQLite statement to be executed.
+    :type stmt: String
+    :return: The list of the results from the SQLite statement
+    :rtype: list
+    """
+    # create a database connection, executes statement, and returns all the results
     try:
         with sqlite3.connect("SPASE_Data.db") as conn:
             cur = conn.cursor()
-            #print(stmt)
             cur.execute(stmt)
             rows = cur.fetchall()
     except sqlite3.Error as e:
@@ -112,5 +192,30 @@ def SDAC_records(yr):
             rows = cur.fetchall()
             for row in rows:
                 print("There are " + str(row[0]) + " records published by SDAC in the year " + yr)
+    except sqlite3.Error as e:
+        print(e)
+        
+def create_Tests():
+    # table to input a 1 or 0 depending on if record meets tested criteria
+    sql = (
+        """CREATE TABLE IF NOT EXISTS Tests ( 
+                SPASE_id TEXT NOT NULL, 
+                has_author INTEGER,
+                has_pub INTEGER,
+                has_pubYr INTEGER,
+                has_dataset INTEGER,
+                has_license INTEGER,
+                has_url INTEGER,
+                has_NASAurl INTEGER,
+                has_PI INTEGER,
+                has_desc INTEGER,
+                has_citation INTEGER,
+                has_compliance INTEGER);""")
+    
+    try:
+        with sqlite3.connect('SPASE_Data.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
     except sqlite3.Error as e:
         print(e)
