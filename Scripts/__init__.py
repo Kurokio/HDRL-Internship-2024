81a0937ad8d41693059c976de280f6f67f97488f
Zach Boquet <zcboquet@gmail.com>
2024-07-18 20:15:58 +0000

    Adding more needed imports

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -2,3 +2,6 @@
 from .SQLiteFun import create_sqlite_database, databaseInfo, FAIRScorer, execution, executionALL
 from .main import Create, View
 from .PlotScripts import FAIR_Chart, MetadataBarChart
+from .SPASE_Scraper_Script import SPASE_Scraper
+from .DatalinkSep import AccessRightsSep
+from .SPASE_DataPrinter import MetadataPrinter
