7639f2ac07ea7b1cb55c0d9c43859f56a033664b
Zach Boquet <zcboquet@gmail.com>
2024-07-19 21:47:47 +0000

    Adding import to new plotting function

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -1,7 +1,7 @@
 # import modules needed for notebooks
 from .SQLiteFun import create_sqlite_database, databaseInfo, FAIRScorer, execution, executionALL
 from .main import Create, View
-from .PlotScripts import FAIR_Chart, MetadataBarChart, MetadataBarChart_NASA_URL
+from .PlotScripts import FAIR_Chart, MetadataBarChart
 from .SPASE_Scraper_Script import SPASE_Scraper
 from .DatalinkSep import AccessRightsSep
 from .SPASE_DataPrinter import MetadataPrinter
